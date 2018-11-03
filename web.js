23e36543bee786f01485de65f12b2f3f72dc1301
David Lurth <dmlurth@gmail.com>
2018-11-03 14:57:47 -0600

    initial

---
+++ web.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var port = 12345;
+var ip = '10.1.0.111';
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+var sitePath = './' + process.argv[2];
+var ct = [];
+ct['.html'] = 'text/html';
+ct['.css'] = 'text/css';
+ct['.js'] = 'application/javascript';
+ct['.json'] = 'application/json';
+ct['.png'] = 'image/png';
+
+var server = http.createServer((req, res) => {
+  console.log(new Date().toJSON() + ' - ' + req.url);
+  var url = req.url;
+  var contenttype;
+  var file = sitePath;
+  if (url === '/')
+    file += '/index.html';
+  else
+    file += url;
+  fs.readFile(file, (err, data) => {
+    if (err)
+      sendResponse(404, 'text/plain', 'not found', res);
+    else {
+      contenttype = ct[path.extname(file)];
+      if (!contenttype)
+        contenttype = 'text/plain';
+      sendResponse(200, contenttype, data, res);
+    }
+  });
+});
+
+function sendResponse(code, contenttype, data, res) {
+  res.writeHead(code, { 'Content-Type': contenttype });
+  res.end(data);
+}
+
+server.listen(port, ip);
+console.log('ready to serve');
